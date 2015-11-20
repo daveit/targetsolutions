7cfa4ead892368941db30d12cd591fd112a08e10
daveit <daveporter@me.com>
2015-11-20 19:04:59 +0800

    added Amazon S3 with Heroku6

--- db/migrate/20151026021220_create_rifles.rb
+++ db/migrate/20151026021220_create_rifles.rb
@@ -7,8 +7,6 @@ class CreateRifles < ActiveRecord::Migration
       t.text :details
       t.boolean :active
       t.references :make, index: true, foreign_key: true
-      t.references :due, index: true, foreign_key: true
-
       t.timestamps null: false
     end
   end
