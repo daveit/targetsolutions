7e047a8ba6f7563b10f1b785758c51e013c2ae5c
“Dave <daveporter@me.com>
2015-12-20 19:06:00 +0800

    rifles fix number to numberic

--- db/migrate/20151220104141_add_style_to_rifles.rb
+++ db/migrate/20151220104141_add_style_to_rifles.rb
@@ -1,6 +1,6 @@
 class AddStyleToRifles < ActiveRecord::Migration
   def change
     add_column :rifles, :style, :string
-    add_column :rifles, :price, :number
+    add_column :rifles, :price, :numeric
   end
 end
